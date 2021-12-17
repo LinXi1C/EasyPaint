05b82459223743af1ccbfd166e70827f90f42f48
linxi1c <1443506887@qq.com>
2021-12-17 17:57:40 +0800

    changes

--- js/canvasAutoSize.js
+++ js/canvasAutoSize.js
@@ -1,9 +1,10 @@
 var CanvasAutoResize = {
     draw: function() {
     var ctx = document.getElementById('myCanvas').getContext('2d');
-    var canvasContainer = document.getElementById('CanvasContainer');
-    ctx.canvas.width  = canvasContainer.offsetWidth-40;
-    ctx.canvas.height = canvasContainer.offsetHeight-2;
+    var canvasContainer = document.getElementById('container');
+
+    ctx.canvas.width  = canvasContainer.offsetWidth-35;
+    ctx.canvas.height = window.screen.availHeight-210;
     },
 
     initialize: function(){
