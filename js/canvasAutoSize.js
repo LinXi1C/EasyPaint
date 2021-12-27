1b4c20008dbcb24544e15dd438a625b04a8b4396
linxi <1443506887@qq.com>
2021-12-27 15:02:07 +0800

    changes

--- js/canvasAutoSize.js
+++ js/canvasAutoSize.js
@@ -1,10 +1,11 @@
 var CanvasAutoResize = {
     draw: function() {
     var ctx = document.getElementById('myCanvas').getContext('2d');
-    var canvasContainer = document.getElementById('container');
+    var canvasContainer = document.getElementById('DrawContainer');
 
     ctx.canvas.width  = canvasContainer.offsetWidth-30;
     ctx.canvas.height = window.screen.availHeight-210;
+
     },
 
     initialize: function(){
@@ -18,4 +19,27 @@ var CanvasAutoResize = {
 
 $(function(argument) {
     CanvasAutoResize.initialize();
+});
+
+var CanvasAutoResize2 = {
+    draw: function() {
+        var ctx = document.getElementById('graph').getContext('2d');
+        var canvasContainer = document.getElementById('FuncContainer');
+
+        ctx.canvas.width  = canvasContainer.offsetWidth-30;
+        ctx.canvas.height = window.screen.availHeight-220;
+
+    },
+
+    initialize: function(){
+        var self = CanvasAutoResize2;
+        self.draw();
+        $(window).on('resize', function(event){
+            self.draw();
+        });
+    }
+}
+
+$(function(argument) {
+    CanvasAutoResize2.initialize();
 });
